c1c33d5fef83b54c6cac2fd2ce342478b3b1440a
s0l0n3t <furkantokgz@gmail.com>
2025-07-08 13:26:58 +0300

    secret link

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ function App() {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await fetch('http://signalseek.xyz:8180/prod/productlist');
+        const response = await fetch(import.meta.env.VITE_API_URL+'/prod/productlist');
         const text = await response.text();
         
         // Extract JSON from the HTML response
